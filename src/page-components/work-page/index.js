329a09644e9a59b5fe75a85c35fb8c5d01dfdb87
Matt <mattparksolutions@gmail.com>
2020-06-13 20:42:43 -0400

    prettier
    Signed-off-by: Matt <mattparksolutions@gmail.com>

--- src/page-components/work-page/index.js
+++ src/page-components/work-page/index.js
@@ -1,42 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { withStyles, Typography } from "@material-ui/core";
+import { withStyles, Typography } from '@material-ui/core'
 
 const WorkPage = withStyles(theme => ({
-  root: {
-  },
-  placeholder: {
-    padding: theme.spacing(5, 0),
-    '& > h3': {
-      marginTop: '10vh',
-      textAlign: 'center',
+    root: {},
+    placeholder: {
+        padding: theme.spacing(5, 0),
+        '& > h3': {
+            marginTop: '10vh',
+            textAlign: 'center',
+        },
     },
-  }
 }))(({ classes }) => {
-  return (
-    <div className={`${classes.root} contentWrapper`}>
-      <Typography variant="h2">Projects</Typography>
-      <div className={classes.placeholder}>
-        <Typography variant={"h3"}>
-          Project 1
-        </Typography>
-        <Typography variant={"h3"}>
-          Project 2
-        </Typography>
-        <Typography variant={"h3"}>
-          Project 3
-        </Typography>
-        <Typography variant={"h3"}>
-          Project 4
-        </Typography>
-        <Typography variant={"h3"}>
-          Project 5
-        </Typography>
-      </div>
-    </div>
-  )
+    return (
+        <div className={`${classes.root} contentWrapper`}>
+            <Typography variant="h2">Projects</Typography>
+            <div className={classes.placeholder}>
+                <Typography variant={'h3'}>Project 1</Typography>
+                <Typography variant={'h3'}>Project 2</Typography>
+                <Typography variant={'h3'}>Project 3</Typography>
+                <Typography variant={'h3'}>Project 4</Typography>
+                <Typography variant={'h3'}>Project 5</Typography>
+            </div>
+        </div>
+    )
 })
 WorkPage.propTypes = {
-  classes: PropTypes.object,
+    classes: PropTypes.object,
 }
 export default WorkPage
