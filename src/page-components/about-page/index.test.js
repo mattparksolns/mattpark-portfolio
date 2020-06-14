19ce315335e0822192aad0e79fb7656c41175e95
Matt <mattparksolutions@gmail.com>
2020-06-13 20:43:05 -0400

    prettier
    Signed-off-by: Matt <mattparksolutions@gmail.com>

--- src/page-components/about-page/index.test.js
+++ src/page-components/about-page/index.test.js
@@ -3,17 +3,17 @@ import renderer from 'react-test-renderer'
 
 import AboutPage from './index'
 
-describe("AboutPage", () => {
-  let tree
+describe('AboutPage', () => {
+    let tree
 
-  beforeEach(() => {
-    tree = renderer.create(<AboutPage />).toJSON()
-  })
+    beforeEach(() => {
+        tree = renderer.create(<AboutPage />).toJSON()
+    })
 
-  // it("renders correctly", () => {
-  //   expect(tree).toMatchSnapshot()
-  // })
-  it("passes", () => {
-    expect(true).toBe(true)
-  })
+    // it("renders correctly", () => {
+    //   expect(tree).toMatchSnapshot()
+    // })
+    it('passes', () => {
+        expect(true).toBe(true)
+    })
 })
