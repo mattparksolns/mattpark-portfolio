dfed80e0d0766ed7ebb8f87b7d2ea173e685cac6
Matt <mattparksolutions@gmail.com>
2020-06-13 20:40:38 -0400

    prettier
    Signed-off-by: Matt <mattparksolutions@gmail.com>

--- src/layout/footer/index.js
+++ src/layout/footer/index.js
@@ -5,21 +5,21 @@ import ContactMe from './contact-me'
 import Copyright from './copyright'
 
 const Footer = withStyles(theme => ({
-  root: {
-    background: `linear-gradient(180deg, ${theme.palette.background.paper}, ${theme.palette.background.default})`,
-  },
-  divider: {
-    backgroundColor: theme.palette.secondary.main,
-    height: 2,
-    marginBottom: 5,
-  },
+    root: {
+        background: `linear-gradient(180deg, ${theme.palette.background.paper}, ${theme.palette.background.default})`,
+    },
+    divider: {
+        backgroundColor: theme.palette.secondary.main,
+        height: 2,
+        marginBottom: 5,
+    },
 }))(({ classes, location }) => {
-  return (
-    <footer className={classes.root}>
-      { location.pathname !== '/contact/' && <ContactMe /> }
-      <Divider className={classes.divider} />
-      <Copyright />
-    </footer>
-  )
+    return (
+        <footer className={classes.root}>
+            {location.pathname !== '/contact/' && <ContactMe />}
+            <Divider className={classes.divider} />
+            <Copyright />
+        </footer>
+    )
 })
 export default Footer
