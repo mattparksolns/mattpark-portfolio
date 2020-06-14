4016bf11a44b54571fbda46fe98b50086df91364
Matt <mattparksolutions@gmail.com>
2020-06-13 20:46:16 -0400

    prettier
    Signed-off-by: Matt <mattparksolutions@gmail.com>

--- src/utils/provider-wrappers.js
+++ src/utils/provider-wrappers.js
@@ -1,16 +1,14 @@
-import React from "react"
-import { Provider as ReduxProvider } from "react-redux"
+import React from 'react'
+import { Provider as ReduxProvider } from 'react-redux'
 
-import configureAppStore from "../store"
-import ThemeProvider from "../themes"
+import configureAppStore from '../store'
+import ThemeProvider from '../themes'
 
 export const RootWrapper = ({ element }) => {
-  const store = configureAppStore()
-  return (
-    <ReduxProvider store={store}>
-      <ThemeProvider>
-        {element}
-      </ThemeProvider>
-    </ReduxProvider>
-  )
+    const store = configureAppStore()
+    return (
+        <ReduxProvider store={store}>
+            <ThemeProvider>{element}</ThemeProvider>
+        </ReduxProvider>
+    )
 }
