0b8768fb4aecfaea5de2ca2718c09ab4eadfa6f0
Matt <mattparksolutions@gmail.com>
2020-06-13 20:48:12 -0400

    prettier
    Signed-off-by: Matt <mattparksolutions@gmail.com>

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,18 +1,18 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react'
 
 export const useLocalStorage = (name, initialValue) => {
-  const windowGlobal = typeof window !== "undefined" && window
-  const [value, setValue] = useState(() => {
-    if (windowGlobal) {
-      const currentValue = windowGlobal.localStorage.getItem(name)
-      return currentValue ? JSON.parse(currentValue) : initialValue
-    }
-    return initialValue
-  });
+    const windowGlobal = typeof window !== 'undefined' && window
+    const [value, setValue] = useState(() => {
+        if (windowGlobal) {
+            const currentValue = windowGlobal.localStorage.getItem(name)
+            return currentValue ? JSON.parse(currentValue) : initialValue
+        }
+        return initialValue
+    })
 
-  useEffect(() => {
-    if (windowGlobal)
-      windowGlobal.localStorage.setItem(name, JSON.stringify(value))
-  }, [name, value, windowGlobal])
-  return [value, setValue]
-};
+    useEffect(() => {
+        if (windowGlobal)
+            windowGlobal.localStorage.setItem(name, JSON.stringify(value))
+    }, [name, value, windowGlobal])
+    return [value, setValue]
+}
--- src/pages/test-page.tsx
+++ src/pages/test-page.tsx
@@ -1,9 +1,9 @@
-import { graphql } from "gatsby"
+import { graphql } from 'gatsby'
 import React from 'react'
 import PropTypes, { InferProps } from 'prop-types'
 import { Typography } from '@material-ui/core'
 
-import SEO from "../components/seo"
+import SEO from '../components/seo'
 
 export const pageQuery = graphql`
     query TestQuery($path: String!) {
@@ -18,26 +18,20 @@ export const pageQuery = graphql`
     }
 `
 
-const TestPage = ({
-  data,
-  location,
-}: InferProps<typeof TestPage.propTypes>) => {
+const TestPage = ({}: InferProps<typeof TestPage.propTypes>) => {
+    // const { markdownRemark: post } = data // data.markdownRemark holds your post data
+    // const { frontmatter, html } = post
 
-  // const { markdownRemark: post } = data // data.markdownRemark holds your post data
-  // const { frontmatter, html } = post
-
-  return (
-    <>
-      <SEO pageTitle="TestPage"/>
-      <Typography variant="h1">Test Page</Typography>
-      <Typography variant="h2">
-        testing shit..
-      </Typography>
-    </>
-  )
+    return (
+        <>
+            <SEO pageTitle="TestPage" />
+            <Typography variant="h1">Test Page</Typography>
+            <Typography variant="h2">testing shit...</Typography>
+        </>
+    )
 }
 TestPage.propTypes = {
-  data: PropTypes.object,
-  location: PropTypes.object
+    data: PropTypes.object,
+    location: PropTypes.object,
 }
 export default TestPage
