dc66bf55bc409cd626ccd422ffdb5e07fe6ae7bc
Matt <mattparksolutions@gmail.com>
2020-06-13 20:39:04 -0400

    prettier
    Signed-off-by: Matt <mattparksolutions@gmail.com>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,21 +1,21 @@
 import './src/styles/global.scss'
-import { RootWrapper } from "./src/utils/provider-wrappers"
+import { RootWrapper } from './src/utils/provider-wrappers'
 export const wrapRootElement = RootWrapper
 
 export const onClientEntry = () => {
-  // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
-  if (!(`IntersectionObserver` in window)) {
-    import(`intersection-observer`)
-    console.log(`# IntersectionObserver is polyfilled!`)
-  }
+    // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
+    if (!(`IntersectionObserver` in window)) {
+        import(`intersection-observer`)
+        console.log(`# IntersectionObserver is polyfilled!`)
+    }
 }
 
 export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `This application has been updated.` +
-    `Reload to display the latest version?`
-  )
-  if (answer === true) {
-    window.location.reload()
-  }
+    const answer = window.confirm(
+        `This application has been updated.` +
+            `Reload to display the latest version?`
+    )
+    if (answer === true) {
+        window.location.reload()
+    }
 }
