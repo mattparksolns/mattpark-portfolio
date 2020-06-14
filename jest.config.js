b203d94ca2e527676ed3de303b0b2f091b6e8c4a
Matt <mattparksolutions@gmail.com>
2020-06-14 02:54:45 -0400

    svg
    Signed-off-by: Matt <mattparksolutions@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
     transform: {
-        '^.+\\.jsx?$': `<rootDir>/jest-preprocess.js`,
+        '^.+\\.jsx?$': `<rootDir>/jest.preprocess.js`,
     },
     moduleNameMapper: {
-        '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
-        '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
+        '.+\\.(css|styl|less|sass|scss|svg)$': `identity-obj-proxy`,
+        '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
+        'socials.js': `<rootDir>/__mocks__/file-mock.js`,
     },
     testPathIgnorePatterns: [
         `node_modules`,
@@ -21,6 +22,6 @@ module.exports = {
     },
     testURL: `http://localhost`,
     verbose: true,
-    setupFiles: [`<rootDir>/loadershim.js`],
-    setupFilesAfterEnv: ['<rootDir>/setup-test-env.js'],
+    setupFiles: [`<rootDir>/jest.loadershim.js`],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 }
