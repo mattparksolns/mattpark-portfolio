a57cd6a5904d7f09ce9e31770bd4a59d01daccfd
Matt <mattparksolutions@gmail.com>
2020-07-03 11:16:36 -0400

    PageQUery

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -1,36 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from 'gatsby'
-import { Helmet } from 'react-helmet'
+import { graphql } from 'gatsby'
 import { Typography } from '@material-ui/core'
 
-const BlogPostTemplate = () => {
-    const data = useStaticQuery(graphql`
-        query BlogPostByPath($slug: String!) {
-            markdownRemark(fields: { slug: { eq: $slug } }) {
-                html
-                frontmatter {
-                    date(formatString: "MMMM DD, YYYY")
-                    path
-                    title
-                }
-            }
-        }
-    `)
+import SEO from '../components/seo'
+
+const BlogPostTemplate = ({ data }) => {
     const { markdownRemark: post } = data // data.markdownRemark holds your post data
-    const { frontmatter, html } = post
+    const { html, excerpt, frontmatter } = post
 
     return (
         <>
-            <Helmet title={frontmatter.title} />
-            <Typography variant="h1">{frontmatter.title}</Typography>
-            <Typography variant="h2">{frontmatter.date}</Typography>
-            <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: html }} />
+            <SEO title={frontmatter.title} description={frontmatter.description || excerpt} />
+            <article>
+                <Typography variant="h1">{frontmatter.title}</Typography>
+                <Typography variant="h2">{frontmatter.date}</Typography>
+                <section className="blog-post-content" dangerouslySetInnerHTML={{ __html: html }} />
+            </article>
         </>
     )
 }
 BlogPostTemplate.propTypes = {
     data: PropTypes.object.isRequired,
 }
-
 export default BlogPostTemplate
+
+export const pageQuery = graphql`
+    query BlogPostBySlug($slug: String!) {
+        markdownRemark(fields: { slug: { eq: $slug } }) {
+            id
+            excerpt(pruneLength: 160)
+            html
+            frontmatter {
+                title
+                path
+                date(formatString: "MMMM DD, YYYY")
+                description
+            }
+        }
+    }
+`
