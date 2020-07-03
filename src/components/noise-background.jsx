fdaf0ce7532c4b61646a325497d541ffd8d1e033
Matt <mattparksolutions@gmail.com>
2020-07-03 07:13:12 -0400

    scripts
    Signed-off-by: Matt <mattparksolutions@gmail.com>

---
+++ src/components/noise-background.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const NoiseBackground = (
+    <div key={'noise-background'} className={'noiseWrapper'}>
+        <div className={'noiseOverlay'} />
+    </div>
+)
+export default NoiseBackground
---
+++ src/themes/theme-script.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Terser from 'terser'
+
+const themeScriptFn = () => {
+    // window.__onThemeChange = () => {}
+    window.__setThemeType = themeType => {
+        window.__theme = themeType
+        window.__onThemeChange()
+        if (themeType === 'dark') {
+            document.body.classList.add('dark')
+        } else {
+            document.body.classList.remove('dark')
+        }
+        try {
+            localStorage.setItem('themeType', themeType)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    let preferredTheme
+    try {
+        preferredTheme = localStorage.getItem('themeType')
+    } catch (error) {
+        console.error(error)
+    }
+    const mql = window.matchMedia('(prefers-color-scheme: dark)')
+    mql.addEventListener('change', event => {
+        window.__setThemeType(event.matches ? 'dark' : 'light')
+    })
+    window.__setThemeType(preferredTheme || (mql.matches ? 'dark' : 'light'))
+}
+
+const themeScriptString = `(${String(themeScriptFn)})()`
+const minifiedThemeScriptString = Terser.minify(themeScriptString).code
+
+const ThemeScript = (
+    <script
+        key={'theme-script'}
+        dangerouslySetInnerHTML={{
+            __html: minifiedThemeScriptString,
+        }}
+    />
+)
+export default ThemeScript
