0618090d7040deaa931a9e4a2fe3fa834802f586
Matt <mattparksolutions@gmail.com>
2020-07-03 07:01:02 -0400

    prettier
    Signed-off-by: Matt <mattparksolutions@gmail.com>

--- backstop_data/engine_scripts/puppet/onReady.js
+++ backstop_data/engine_scripts/puppet/onReady.js
@@ -1,6 +1,7 @@
+// eslint-disable-next-line no-unused-vars
 module.exports = async (page, scenario, vp) => {
-  console.log('SCENARIO > ' + scenario.label);
-  await require('./clickAndHoverHelper')(page, scenario);
+    console.log('SCENARIO > ' + scenario.label)
+    await require('./clickAndHoverHelper')(page, scenario)
 
-  // add more ready handlers here...
-};
+    // add more ready handlers here...
+}
