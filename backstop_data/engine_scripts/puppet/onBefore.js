1356618d691e3c4e728164d719c5afd541a35566
Matt <mattparksolutions@gmail.com>
2020-07-03 07:00:53 -0400

    prettier
    Signed-off-by: Matt <mattparksolutions@gmail.com>

--- backstop_data/engine_scripts/puppet/onBefore.js
+++ backstop_data/engine_scripts/puppet/onBefore.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 module.exports = async (page, scenario, vp) => {
-  await require('./loadCookies')(page, scenario);
-};
+    await require('./loadCookies')(page, scenario)
+}
