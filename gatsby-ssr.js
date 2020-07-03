88b225d6839efe43c2a2ce3c2b5a325d3d80698c
Matt <mattparksolutions@gmail.com>
2020-07-03 06:40:27 -0400

    gatsby files: prettier
    Signed-off-by: Matt <mattparksolutions@gmail.com>

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,51 +1,9 @@
-import { RootWrapper } from './src/utils/provider-wrappers'
+import RootWrapper from './src/components/root-wrapper'
 export const wrapRootElement = RootWrapper
 
-import React from 'react'
-import Terser from 'terser'
+import ThemeScript from './src/themes/theme-script'
+import NoiseBackground from './src/components/noise-background'
+
 export const onRenderBody = ({ setPreBodyComponents }) => {
-    let themeScriptFn = () => {
-        window.__onThemeChange = () => {}
-        window.__setThemeType = themeType => {
-            window.__theme = themeType
-            window.__onThemeChange()
-            if (themeType === 'dark') {
-                document.body.classList.add('dark')
-            } else {
-                document.body.classList.remove('dark')
-            }
-            try {
-                localStorage.setItem('themeType', themeType)
-            } catch (error) {
-                console.error(error)
-            }
-        }
-        let preferredTheme
-        try {
-            preferredTheme = localStorage.getItem('themeType')
-        } catch (error) {
-            console.error(error)
-        }
-        const mql = window.matchMedia('(prefers-color-scheme: dark)')
-        mql.addEventListener('change', e => {
-            window.__setThemeType(e.matches ? 'dark' : 'light')
-        })
-        window.__setThemeType(
-            preferredTheme || (mql.matches ? 'dark' : 'light')
-        )
-    }
-    themeScriptFn = `(${String(themeScriptFn)})()`
-    themeScriptFn = Terser.minify(themeScriptFn).code
-    const ThemeScript = (
-        <script
-            key={'theme-script'}
-            dangerouslySetInnerHTML={{ __html: themeScriptFn }}
-        />
-    )
-    const NoiseBackground = (
-        <div key={'noise-background'} className={'noiseWrapper'}>
-            <div className={'noiseOverlay'} />
-        </div>
-    )
     setPreBodyComponents([ThemeScript, NoiseBackground])
 }
