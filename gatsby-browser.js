a3d5f9c1cc2b00134b0199f213085b56e7a1fa13
Matt <mattparksolutions@gmail.com>
2020-07-03 15:13:57 -0400

    automatically reload service worker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,8 +12,9 @@ export const onClientEntry = () => {
 }
 
 export const onServiceWorkerUpdateReady = () => {
-    const answer = window.confirm(`This application has been updated.` + `Reload to display the latest version?`)
-    if (answer) {
-        window.location.reload()
-    }
+    // const answer = window.confirm(`This application has been updated.` + `Reload to display the latest version?`)
+    // if (answer) {
+    //     window.location.reload()
+    // }
+    window.location.reload()
 }
