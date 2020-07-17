c845aaf402e5e11cd6cd46ece540d860ce7b3cdd
Matt <mattparksolutions@gmail.com>
2020-07-17 03:13:22 -0400

    added contentWrapper div for margins

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,13 +5,13 @@ import SEO from '../components/seo'
 
 const NotFoundPage = () => {
     return (
-        <>
-            <SEO pageTitle="404: Not found" />
-            <Typography variant="h2" component="p">
+        <div className={`contentWrapper`}>
+            <SEO pageTitle={'404: Not found'} />
+            <Typography variant={'h2'} component={'p'}>
                 404 NOT FOUND
             </Typography>
-            <Typography variant="h6">You just hit a route that doesn&#39;t exist...</Typography>
-        </>
+            <Typography variant={'h6'}>You just hit a route that doesn&#39;t exist...</Typography>
+        </div>
     )
 }
 export default NotFoundPage
